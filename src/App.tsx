83264b04d7ba197b443fc5ae01b75ecc50728a94
Alfred <ealfred828@gmail.com>
2024-11-25 12:32:19 +0100

    initialized the project using react typescript

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+// import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <div className="bg-white h-full w-full text-red-500"> Alfred </div>
+    </>
+  );
+}
+
+export default App;
