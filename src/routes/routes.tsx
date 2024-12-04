8d478b6b8a83774a4f89ec084917b0c86bd30396
Alfred <ealfred828@gmail.com>
2024-12-04 16:15:26 +0100

    still on the deployment errors

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,11 +1,15 @@
 import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import ProtectedRoute from "./protectedRoutes";
+import Home from "../pages/Home.tsx";
+import About from "../pages/About.tsx";
+import Dashboard from "../pages/Dashboard.tsx";
+import SignUp from "../pages/SignUp.tsx";
 
-const Home = lazy(() => import("../pages/Home.tsx"));
-const About = lazy(() => import("../pages/About.tsx"));
-const Dashboard = lazy(() => import("../pages/Dashboard.tsx"));
-const Register = lazy(() => import("../pages/SignUp.tsx"))
+// const Home = lazy(() => import("../pages/Home.tsx"));
+// const About = lazy(() => import("../pages/About.tsx"));
+// const Dashboard = lazy(() => import("../pages/Dashboard.tsx"));
+// const Register = lazy(() => import("../pages/SignUp.tsx"))
 
 const AppRoutes = () => {
   return (
@@ -14,7 +18,7 @@ const AppRoutes = () => {
         {/* Public Routes */}
 
         <Route path="/" element={<Home />} />
-        <Route path="/sign_up" element={<Register />} />
+        <Route path="/sign_up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
 
         {/* Protected Routes */}
