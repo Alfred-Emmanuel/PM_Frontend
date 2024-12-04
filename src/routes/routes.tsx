c432b9cb7514f7b720259d1af4b4b5521d7fa86a
Alfred <ealfred828@gmail.com>
2024-12-04 16:03:34 +0100

    Fixing some deployment issues

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,10 +2,10 @@ import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import ProtectedRoute from "./protectedRoutes";
 
-const Home = lazy(() => import("../pages/Home"));
-const About = lazy(() => import("../pages/About"));
-const Dashboard = lazy(() => import("../pages/Dashboard"));
-const Register = lazy(() => import("../pages/SignUp"))
+const Home = lazy(() => import("../pages/Home.tsx"));
+const About = lazy(() => import("../pages/About.tsx"));
+const Dashboard = lazy(() => import("../pages/Dashboard.tsx"));
+const Register = lazy(() => import("../pages/SignUp.tsx"))
 
 const AppRoutes = () => {
   return (
